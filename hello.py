0ddf882d3debfec719ffcb2780ea5ff6a7db2445
Felix <luofancxh@foxmail.com>
2019-03-08 09:20:27 +0800

    no message

--- hello.py
+++ hello.py
@@ -1,3 +1,149 @@
-print("hello world!")
-print(2**10)
-exit()
+# !/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# print("hello", "world!")
+# print(2**10)
+# name=input("input you name:")
+# print("hello",name)
+# print("1024*768=",1024*768)
+
+# a=input("input value a=")
+
+# a=1.2e-1;
+# if a>0:
+#     print(a)
+# else:
+#     print(-a)
+
+# print('\\\/\\')
+# print(r'\\\/'+"\\")
+
+# print(r'''dear
+# god
+# forgive
+# me./n
+# ''')
+
+# age=int(input("input you age:"))
+# if age>=18:
+#     print('成年');
+# else:
+#     print("未成年");
+
+# a = 'ABC'
+# b = a
+# a = 'XYZ'
+# print(b)
+
+# print(10/3)
+# print(9/3)
+# #地板除
+# print(10//3)
+
+# print("n =" ,123)
+# print(r"f = 456.789")
+# print("s1","=","Hello,","world")
+# print("s2","=",r"Hello, \'Adam\''")
+# print("s3 = ",r"r'''Hello,")
+# print("Lisa!'''")
+
+# print(ord('1'))
+# print(chr(25105))
+# print(chr(29233))    
+# print(chr(20320)) 
+
+# print('A'.encode('ascii'))
+# print('中文'.encode('utf-8'))
+
+# print("hi ,%s , %s,you are a big cat." % ('jack','ss'))
+
+# print('%03d-%03d' % (33.31, 1))
+# print('%2.3f' % 123.1415926)
+
+# print("growth rate : %d %%" % 7 )
+
+# print('Hello, {0}, 成绩提升了 {1:.1f}%'.format('小明', 17.125))
+
+# s1=72
+# s2=85
+# r=85-72
+# print('小米成绩提升了:%.1f%%' % r)
+
+# classmate=["A","B","C"]
+# print(classmate)
+# print(len(classmate))
+
+# L = [
+#     ['Apple', 'Google', 'Microsoft'],
+#     ['Java', 'Python', 'Ruby', 'PHP'],
+#     ['Adam', 'Bart', 'Lisa']
+# ]
+# print(L[0][1])
+# print(L[1][1])
+# print(L[2][2])
+
+# age = 20
+# if age >= 6:
+#     print('teenager')
+# elif age >= 18:
+#     print('adult')
+# else:
+#     print('kid')
+
+# if 0:
+#     print(11)
+# else:
+#     print(22)
+
+# print("欢迎使用BMI计算器")
+# height =float(input("输入你的身高(m):"))
+# weight =float(input("输入你的体重(kg):"))
+# bmi=weight/(height**2)
+# if bmi<=18.5:
+#     print("过轻")
+# elif bmi<=25:
+#     print("正常")
+# elif bmi<=28:
+#     print("过重")
+# elif bmi<32:
+#     print("肥胖")
+# elif bmi>=32:
+#     print("严重肥胖")
+
+# names = ['Michael', 'Bob', 'Tracy']
+# for name in names:
+#     print(name)
+
+L = ['Bart', 'Lisa', 'Adam']
+# for l in L:
+#     print("Hello, %s%s" % (l,'!'))
+
+# i=len(L)-1;
+# while i>=0:   
+#     print("hello,",L[i],"!")
+#     i=i-1
+
+# y=0
+# while y<len(L):
+#     print("hello,",L[y],"!")
+#     y=y+1
+
+# n=0
+# while n<=100:    
+#     print(n)
+#     n=n+1
+#     if(n>10):
+#         break
+# print("end")
+
+
+n=0
+while n<=10:
+    n=n+1
+    if(n%2==0):
+        continue
+    print(n)
+
+    
+    
+
